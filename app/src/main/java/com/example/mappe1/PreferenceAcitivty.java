8967f3d4ea7007f6736cc8ce1448d8cc05e47bff
even996 <evenolsen96@gmail.com>
2019-09-16 09:37:38 +0200

    Lagt til tysk og norsk på imagebutton

--- app/src/main/java/com/example/mappe1/PreferenceAcitivty.java
+++ app/src/main/java/com/example/mappe1/PreferenceAcitivty.java
@@ -1,11 +1,17 @@
 package com.example.mappe1;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class PreferenceAcitivty extends AppCompatActivity {
 
@@ -13,9 +19,9 @@ public class PreferenceAcitivty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preference_activity);
-
-
         Button prefReturnBtn = findViewById(R.id.returnBtn);
+        ImageButton imageButtonNor = findViewById(R.id.imageNor);
+        ImageButton imageButtonGer = findViewById(R.id.imageGer);
 
 
        prefReturnBtn.setOnClickListener(new View.OnClickListener() {
@@ -25,13 +31,45 @@ public class PreferenceAcitivty extends AppCompatActivity {
            }
        });
 
+       imageButtonNor.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               norwergian(view);
+           }
+       });
+
+        imageButtonGer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                german(view);
+            }
+        });
 
     }
+
 
 
     public void menu(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void setLanguage(String country){
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration cf = res.getConfiguration();
+        cf.setLocale(new Locale(country));
+        res.updateConfiguration(cf,dm);
+    }
+
+    public void german(View view){
+        setLanguage("de");
+        recreate();
+    }
+
+    public void norwergian(View view){
+        setLanguage("no");
+        recreate();
     }
 
 
