4208fe0ce4a2a4262dfe3a4ebfdc240b1a65fbf1
redhotk1m <kim@adsl.no>
2019-09-18 12:05:44 +0200

    Skrev test i main

--- app/src/main/java/com/example/mappe1/MainActivity.java
+++ app/src/main/java/com/example/mappe1/MainActivity.java
@@ -19,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        System.out.println("TEST!");
+
+
         System.out.println((getIntent().hasExtra(EXTRA_NUMBER)+ "test123"));
         Intent intent = getIntent();
         checked = intent.getIntExtra(PreferenceAcitivty.EXTRA_NUMBER, 5);
